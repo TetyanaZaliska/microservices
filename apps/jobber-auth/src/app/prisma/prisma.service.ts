c21e3007bf69bb636d9886b215f3b49dec771ba4
Tetiana <tetiana@MacBook-Air-Tetiana.local>
2026-03-19 13:26:25 -0700

    added prisma7, lib and graphql

---
+++ apps/jobber-auth/src/app/prisma/prisma.service.ts
@@ -0,0 +1,14 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma-clients/jobber-auth/client';
+//import { PrismaPg } from '@prisma/adapter-pg';
+//import { PrismaClient } from '../../../prisma/generated/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+    async onModuleInit() {
+       // const connectionString = process.env.AUTH_DATABASE_URL;
+        //const adapter = new PrismaPg({ connectionString });
+        
+        await this.$connect();
+    }
+}
