6d42652fe2fe6c385175512a401132c4307f2b1d
Tetiana <tetiana@MacBook-Air-Tetiana.local>
2026-03-19 16:36:20 -0700

    fixed prisma 7 bugs

--- apps/jobber-auth/src/app/prisma/prisma.service.ts
+++ apps/jobber-auth/src/app/prisma/prisma.service.ts
@@ -1,10 +1,22 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma-clients/jobber-auth/client';
-//import { PrismaPg } from '@prisma/adapter-pg';
-//import { PrismaClient } from '../../../prisma/generated/client';
+//import { PrismaClient } from '@prisma-clients/jobber-auth/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../../../prisma/generated/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+    constructor() {
+        const connectionString = process.env.AUTH_DATABASE_URL;
+
+        const adapter = new PrismaPg({
+            connectionString,
+        });
+
+        super({
+            adapter,
+        });
+    }
+
     async onModuleInit() {
        // const connectionString = process.env.AUTH_DATABASE_URL;
         //const adapter = new PrismaPg({ connectionString });
