a5a97b81f0d1cc7d78d0090265e568d0e889c6d3
Tetiana <tetiana@MacBook-Air-Tetiana.local>
2026-03-20 18:31:25 -0700

    changed prisma 7 setting for correct work

--- apps/jobber-auth/src/app/users/users.service.ts
+++ apps/jobber-auth/src/app/users/users.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { Prisma } from '../../../prisma/generated/client';
+import { hash } from 'bcrypt';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(private readonly prismaService: PrismaService) {}
+
+    async createUser(data: Prisma.UserCreateInput) {
+        return this.prismaService.user.create({
+            data: {
+                ...data,
+                password: await hash(data.password, 10),
+            },
+        });
+    }
+}
